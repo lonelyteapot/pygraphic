533ab776965a3a4274dd92e363b4b65b9d7b2cc4
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-10 11:45:36 +0300

    Add option for named/unnamed queries

--- pygraphic/gql_query.py
+++ pygraphic/gql_query.py
@@ -3,9 +3,12 @@ from .gql_type import GQLType
 
 class GQLQuery(GQLType):
     @classmethod
-    def get_query_string(cls) -> str:
+    def get_query_string(cls, named: bool = True) -> str:
         def _gen():
-            yield "query {"
+            if named:
+                yield "query " + cls.__name__ + " {"
+            else:
+                yield "query {"
             for line in cls.generate_query_lines():
                 yield line
             yield "}"
--- tests/test_query_generation.py
+++ tests/test_query_generation.py
@@ -3,8 +3,13 @@ from pathlib import Path
 from .get_all_users import GetAllUsers
 
 
+def test_unnamed_query_generation(script_directory: Path):
+    with open(script_directory / "expected_unnamed_query.gql", encoding="utf-8") as fp:
+        expected_query = fp.read()
+    assert GetAllUsers.get_query_string(named=False) == expected_query
+
+
 def test_query_generation(script_directory: Path):
     with open(script_directory / "expected_query.gql", encoding="utf-8") as fp:
         expected_query = fp.read()
-    query_string = GetAllUsers.get_query_string()
-    assert query_string == expected_query
+    assert GetAllUsers.get_query_string() == expected_query
