8ed66c5d668e8f8ede122be15eeb782727943c04
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-10 13:26:09 +0300

    Add test for pydantic parse_obj

--- tests/test_server_interaction.py
+++ tests/test_server_interaction.py
@@ -7,3 +7,10 @@ def test_local_query_execution():
     result = schema.execute_sync(query)
     assert result.errors is None
     assert result.data is not None
+
+
+def test_pydantic_parse_obj():
+    query = GetAllUsers.get_query_string()
+    result = schema.execute_sync(query)
+    assert type(result.data) is dict
+    result = GetAllUsers.parse_obj(result.data)
