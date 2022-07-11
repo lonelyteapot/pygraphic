abc1a875cf807ad7eddd9cb58553812f38e6bfb0
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-11 15:09:49 +0300

    Add minimal_query example

---
+++ examples/minimal_query/get_all_users.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from uuid import UUID
+
+from pygraphic import GQLQuery, GQLType
+
+
+class User(GQLType):
+    id: UUID
+    username: str
+
+
+class GetAllUsers(GQLQuery):
+    users: list[User]
