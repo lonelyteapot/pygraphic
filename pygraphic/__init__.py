f5af639838095b004fe314eb21ecb56bb66ccf70
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-15 22:24:29 +0300

    Fix missing defaults export

--- pygraphic/__init__.py
+++ pygraphic/__init__.py
@@ -1,7 +1,7 @@
-from . import types
+from . import defaults, types
 from ._gql_parameters import GQLParameters
 from ._gql_query import GQLQuery
 from ._gql_type import GQLType
 
 
-__all__ = ["GQLParameters", "GQLType", "GQLQuery", "types"]
+__all__ = ["defaults", "GQLParameters", "GQLType", "GQLQuery", "types"]
