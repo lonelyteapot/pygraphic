5c833d707d84a08a36c602ef4f55db7991456e9c
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-15 22:37:04 +0300

    Fix missing GQLParameters constructor autocompletion

--- pygraphic/_gql_parameters.py
+++ pygraphic/_gql_parameters.py
@@ -4,14 +4,18 @@ from typing import Any
 
 import pydantic
 import pydantic.main
+from pydantic.fields import Field, FieldInfo
+from pydantic.main import __dataclass_transform__
 
 from .defaults import default_alias_generator
 
 
+@__dataclass_transform__(kw_only_default=True, field_descriptors=(Field, FieldInfo))
 class ModelMetaclass(pydantic.main.ModelMetaclass):
     def __getattr__(cls, __name: str) -> Any:
         try:
-            return cls.__fields__[__name]
+            model: type[GQLParameters] = cls  # type: ignore
+            return model.__fields__[__name]
         except KeyError:
             raise AttributeError(
                 f"type object '{cls.__name__}' has no attribute '{__name}'"
