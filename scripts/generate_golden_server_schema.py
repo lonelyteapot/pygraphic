8b571aedcb7158f6f34dbcf31f723505e4b86fff
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-23 19:51:45 +0300

    Delete old examples and tests

--- scripts/generate_golden_server_schema.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from examples.server import server_schema
-
-
-if __name__ == "__main__":
-    Path("golden_files", "server_schema.gql").write_text(
-        server_schema.as_str(), encoding="utf-8"
-    )
