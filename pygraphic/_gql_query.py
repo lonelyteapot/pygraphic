9c612be27b13eeddb0501d406e3d06e40fae69d0
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-23 21:59:45 +0300

    Refactor _get_variables_string

--- pygraphic/_gql_query.py
+++ pygraphic/_gql_query.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterator, Optional
+from typing import Optional
 
 import pydantic
 
 from ._gql_type import GQLType
 from ._gql_variables import GQLVariables
-from ._utils import first_only
 from .types import class_to_graphql_type
 
 
@@ -23,7 +22,7 @@ class GQLQuery(GQLType):
 
         def _gen():
             if named:
-                variables_str = "".join(_gen_variables_string(variables))
+                variables_str = _get_variables_string(variables)
                 yield "query " + cls.__name__ + variables_str + " {"
             else:
                 yield "query {"
@@ -37,15 +36,14 @@ class GQLQuery(GQLType):
         variables: Optional[type[GQLVariables]] = None
 
 
-def _gen_variables_string(variables: Optional[type[GQLVariables]]) -> Iterator[str]:
+def _get_variables_string(variables: Optional[type[GQLVariables]]) -> str:
     if variables is None or not variables.__fields__:
-        return
-    yield "("
-    for field, is_first in zip(variables.__fields__.values(), first_only()):
-        if not is_first:
-            yield ", "
-        yield "$"
-        yield field.alias
-        yield ": "
-        yield class_to_graphql_type(field.type_, allow_none=field.allow_none)
-    yield ")"
+        return ""
+
+    def _generate():
+        for field in variables.__fields__.values():
+            yield "$" + field.alias + ": " + class_to_graphql_type(
+                field.type_, allow_none=field.allow_none
+            )
+
+    return "(" + ", ".join(_generate()) + ")"
