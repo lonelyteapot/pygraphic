5f4e911f40b1299f29bc1c94971918f24c8430da
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-23 19:27:28 +0300

    Create "inline fragments" example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,3 +10,14 @@ def test_arguments():
     assert golden_query == query_str
     assert result.repository.url
     assert result.repository.pull_requests.nodes
+
+
+def test_inline_fragments():
+    golden_query = (
+        Path("golden_files", "example_inline_fragments.gql").read_text("utf-8").strip()
+    )
+    from examples.inline_fragments import Repository, query_str, result
+
+    assert golden_query == query_str
+    assert len(result.search.nodes) == 10
+    assert all(type(node) is Repository for node in result.search.nodes)
