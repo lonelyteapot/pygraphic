94834d13dcce2199c34635893651da2c1133324e
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-24 17:53:08 +0300

    Rewrite tests for serializers.py

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,32 +1,60 @@
+from enum import Enum, auto
+
 import pytest
 
+from pygraphic import GQLVariables
+from pygraphic.serializers import (
+    class_to_graphql,
+    register_graphql_type,
+    value_to_graphql,
+)
 
-# TODO Rewrite tests for serializers
 
-# from pygraphic.types import class_to_graphql_type, register_graphql_type
+def test_basic_scalars_to_graphql():
+    assert value_to_graphql(0) == "0"
+    assert value_to_graphql(0.0) == "0.0"
+    assert value_to_graphql("foo") == '"foo"'
+    assert value_to_graphql(False) == "false"
+    assert value_to_graphql(True) == "true"
+    assert value_to_graphql(None) == "null"
 
 
-@pytest.mark.skip("Code was migrated")
-def test_class_to_typename_crashes_on_unknown_class():
+def test_enum_to_graphql():
+    class Foo(Enum):
+        BAR = auto()
+
+    assert value_to_graphql(Foo.BAR) == "BAR"
+
+
+def test_variable_name_to_graphql():
+    class Variables(GQLVariables):
+        foo: int
+
+    assert value_to_graphql(Variables.foo) == "$foo"
+
+
+def test_basic_nullable_scalar_types_to_graphql():
+    assert class_to_graphql(int, allow_none=True) == "Int"
+    assert class_to_graphql(float, allow_none=True) == "Float"
+    assert class_to_graphql(str, allow_none=True) == "String"
+    assert class_to_graphql(bool, allow_none=True) == "Boolean"
+
+
+def test_basic_non_nullable_scalar_types_to_graphql():
+    assert class_to_graphql(int, allow_none=False) == "Int!"
+    assert class_to_graphql(float, allow_none=False) == "Float!"
+    assert class_to_graphql(str, allow_none=False) == "String!"
+    assert class_to_graphql(bool, allow_none=False) == "Boolean!"
+
+
+def test_fail_of_unkown_type_to_graphql():
     with pytest.raises(TypeError):
-        class_to_graphql_type(object, allow_none=True)
+        class_to_graphql(object, allow_none=True)
+    with pytest.raises(TypeError):
+        class_to_graphql(object, allow_none=False)
 
 
-@pytest.mark.skip("Code was migrated")
-def test_class_to_typename_with_default_scalars():
-    assert class_to_graphql_type(int, allow_none=True) == "Int"
-    assert class_to_graphql_type(float, allow_none=True) == "Float"
-    assert class_to_graphql_type(str, allow_none=True) == "String"
-    assert class_to_graphql_type(int, allow_none=True) == "Int"
-    # assert class_to_graphql_type(str, allow_none=True) == 'ID'
-
-
-@pytest.mark.skip("Code was migrated")
-def test_class_to_typename_with_non_nullable():
-    assert class_to_graphql_type(int, allow_none=False) == "Int!"
-
-
-@pytest.mark.skip("Code was migrated")
-def test_class_to_typename_with_registered_type():
-    register_graphql_type("CustomType", object)
-    assert class_to_graphql_type(object, allow_none=True) == "CustomType"
+def test_custom_registered_type_to_graphql():
+    register_graphql_type("Object", object)
+    assert class_to_graphql(object, allow_none=True) == "Object"
+    assert class_to_graphql(object, allow_none=False) == "Object!"
