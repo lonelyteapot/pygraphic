334bdb619c2dae722a2ee5bfd9791422ae3b6282
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-24 15:38:04 +0300

    Create tests for variable defaults

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -108,8 +108,7 @@ def test_generation_of_named_query_with_variables():
     assert gql == "query Query($i: Int!, $f: Float!, $s: String, $b: Boolean) {\n}"
 
 
-@pytest.mark.skip("Default GraphQL arguments are not yet implemented")
-def test_generation_of_query_with_default_arguments():
+def test_generation_of_query_with_default_variables():
     class Variables(GQLVariables):
         i: int = Field(default=0)
         f: float = Field(default=0.0)
@@ -124,6 +123,30 @@ def test_generation_of_query_with_default_arguments():
     assert gql == (
         'query($i: Int!, $f: Float!, $s: String = "foo", $b: Boolean = null) {\n}'
     )
+
+
+def test_generation_of_variables_excludes_defaults():
+    class Variables(GQLVariables):
+        i: int = Field(default=0)
+        f: float = Field(default=0.0)
+        s: Optional[str] = Field(default="foo")
+        b: Optional[bool] = Field(default=None)
+
+    variables = Variables(b=None)
+    json = variables.json()
+    assert json == '{"i": 0, "f": 0.0, "b": null}'
+
+
+def test_generation_of_variables_without_excluding_defaults():
+    class Variables(GQLVariables):
+        i: int = Field(default=0)
+        f: float = Field(default=0.0)
+        s: Optional[str] = Field(default="foo")
+        b: Optional[bool] = Field(default=None)
+
+    variables = Variables(b=None)
+    json = variables.json(exclude_defaults=False)
+    assert json == '{"i": 0, "f": 0.0, "s": "foo", "b": null}'
 
 
 def test_passing_variables_as_field_arguments():
